230dfe653c13a1c82f9bf75f491a29661d0ee8bf
Sebastian Velandia <sebastian.velandia@blackboard.com>
2020-09-15 20:05:50 -0500

    set credentials and create s3 bucket

--- test.tf
+++ test.tf
@@ -3,7 +3,7 @@ provider "aws" {
     region = "us_west-1"
 }
 
-resourse "aws_s3_bucket" "shebas_tf"{
+resource "aws_s3_bucket" "shebas_tf"{
     buecket = "shebas_buecket_mdt"
     acl     = "public-read-write"
 }
