2aecd2f34ab5976dc7a190bda457a70191c15af0
Sebastian Velandia <sebastian.velandia@blackboard.com>
2020-09-18 22:35:42 -0500

    add sgroup

--- mdle.tf
+++
@@ -1,9 +0,0 @@
-provider "aws" {
-    profile = "default"
-    region = "us-east-1"
-}
-
-resource "aws_s3_bucket" "shebas_tf"{
-    bucket = "shebas-bucket-mdt-2020-09-15"
-    acl     = "private"
-}
--- prod_mdle.tf
+++ prod_mdle.tf
@@ -5,8 +5,36 @@ provider "aws" {
 }
 
 resource "aws_s3_bucket" "shebas_tf"{ # type of the resource and the name of the resource (name isfor tf)
-    bucket = "shebas-bucket-mdt-2020-09-15" # name of the bucket
+    bucket  = "shebas-bucket-mdt-2020-09-15" # name of the bucket
     acl     = "private" # politic of the bucket
 }
 
 resource "aws_default_vpc" "default"{}
+
+resource "aws_security_group" "prod_mdle" {
+    name        = "prod_web"
+    description = "Allow http and https ports"
+
+    ingress {
+        from_port   = 80
+        to_port     = 80
+        protocol     = "tpc"
+        cidr_blocks = ["0.0.0.0/0"] #ips range that it will be permited
+    }
+    ingress {
+        from_port   = 443
+        to_port     = 443
+        protocol     = "tpc"
+        cidr_blocks = ["0.0.0.0/0"] #ips range that it will be permited
+    }
+    egress {
+        from_port   = 0 # no restrictions
+        to_port     = 0 
+        protocol     = "-1" # enabled all protocols 
+        cidr_blocks = ["0.0.0.0/0"] #ips range that it will be permited
+    }
+
+    tags = {
+        "terraform" : "true"
+    }
+}
