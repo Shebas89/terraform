53d7d1c62610135020673684ef097350a3ab7a41
Sebastian Velandia <sebastian.velandia@blackboard.com>
2020-09-27 00:43:57 -0500

    modify the public AZ and subnet and added a new sg

--- prod_mdle.tf
+++ prod_mdle.tf
@@ -35,15 +35,18 @@ resource "aws_nat_gateway" "prod_mdle_nw" {
 
     allocation_id = aws_eip.prod_mdle_eip_nw[count.index].id
     subnet_id     = aws_subnet.prod_mdle_az_private[count.index].id
+
+    tags = {
+        Name = "Nats Gateway prod_mdle"
+        "Terraform" : "true"
+    }
 }
 
 # Public Subnet
 resource "aws_subnet" "prod_mdle_az_public" {
-    count = length(var.cidr_block_az_public)
-
     vpc_id              = aws_vpc.prod_mdle_vpc.id
-    cidr_block          = var.cidr_block_az_public[count.index]
-    availability_zone   = var.az_public[count.index]
+    cidr_block          = var.cidr_block_az_public
+    availability_zone   = var.az_public
     tags = {
         Name = "public_sn"
         "Terraform" : "true"
@@ -103,15 +106,13 @@ resource "aws_route_table" "prod_mdle_private_rt" {
 resource "aws_route_table_association" "prod_mdle_private_rta" {
     count = length(var.cidr_block_az_private)
 
-    subnet_id         = aws_subnet.prod_mdle_az_public[count.index].id
-    route_table_id    = aws_route_table.prod_mdle_public_rt.id
+    subnet_id         = aws_subnet.prod_mdle_az_private[count.index].id
+    route_table_id    = aws_route_table.prod_mdle_private_rt[count.index].id
 }
 
 resource "aws_route_table_association" "prod_mdle_public_rta" {
-    count = length(var.cidr_block_az_public)
-
-    subnet_id         = aws_subnet.prod_mdle_az_private[count.index].id
-    route_table_id    = aws_route_table.prod_mdle_private_rt[count.index].id 
+    subnet_id         = aws_subnet.prod_mdle_az_public.id
+    route_table_id    = aws_route_table.prod_mdle_public_rt.id 
 }
 
 # NACLs of vpc
@@ -139,8 +140,33 @@ resource "aws_s3_bucket" "prod_mdle_s3"{
 }
 
 # Security group
-resource "aws_security_group" "prod_mdle_sg" {
-    name        = "prod_web"
+resource "aws_security_group" "prod_mdle_sg_public" {
+    name        = "prod_web_public"
+    description = "Allow http and https ports"
+    vpc_id      = aws_vpc.prod_mdle_vpc.id
+
+    ingress {
+        from_port   = 0 # no restrictions
+        to_port     = 0 
+        protocol     = "-1" # enabled all protocols 
+        cidr_blocks = var.cidr_block_l0 
+    }
+    egress {
+        from_port   = 0 # no restrictions
+        to_port     = 0 
+        protocol     = "-1" # enabled all protocols 
+        cidr_blocks = var.cidr_block_l0 
+    }
+
+    tags = {
+        Name        = "Security Group Mdle public"
+        "terraform" : "true"
+    }
+}
+
+# Security group
+resource "aws_security_group" "prod_mdle_sg_private" {
+    name        = "prod_web_private"
     description = "Allow http and https ports"
     vpc_id      = aws_vpc.prod_mdle_vpc.id
 
@@ -170,14 +196,14 @@ resource "aws_security_group" "prod_mdle_sg" {
     }
 
     tags = {
-        Name        = "Security Group Mdle"
+        Name        = "Security Group Mdle private"
         "terraform" : "true"
     }
 }
 
 #private instance
 resource "aws_instance" "prod_mdle_ec2_private" {
-    count   = length(var.az_public)
+    count   = length(var.az_private)
 
     ami               = "ami-062ca315b459c9e61" # "ami-02f3e801651bd39bc" 
     instance_type     = "t2.micro"
@@ -185,7 +211,7 @@ resource "aws_instance" "prod_mdle_ec2_private" {
     subnet_id         = aws_subnet.prod_mdle_az_private[count.index].id
 
     vpc_security_group_ids = [
-        aws_security_group.prod_mdle_sg.id
+        aws_security_group.prod_mdle_sg_private.id
     ]
 
     tags = {
@@ -198,11 +224,11 @@ resource "aws_instance" "prod_mdle_ec2_private" {
 resource "aws_instance" "prod_mdle_ec2_public" {
     ami               = "ami-062ca315b459c9e61"
     instance_type     = "t2.micro"
-    availability_zone = aws_subnet.prod_mdle_az_public.0.availability_zone
-    subnet_id         = aws_subnet.prod_mdle_az_public.0.id
+    availability_zone = aws_subnet.prod_mdle_az_public.availability_zone
+    subnet_id         = aws_subnet.prod_mdle_az_public.id
 
     vpc_security_group_ids = [
-        aws_security_group.prod_mdle_sg.id
+        aws_security_group.prod_mdle_sg_public.id
     ]
 
     tags = {
@@ -240,7 +266,7 @@ resource "aws_elb" "prod_mdle_elb" {
     name            = "prod-mdle"
     instances       = aws_instance.prod_mdle_ec2_private.*.id
     subnets         = [ aws_subnet.prod_mdle_az_private.0.id, aws_subnet.prod_mdle_az_private.1.id ]
-    security_groups = [ aws_security_group.prod_mdle_sg.id ]
+    security_groups = [ aws_security_group.prod_mdle_sg_private.id ]
     
     listener {
         instance_port       = 80
--- variables.tf
+++ variables.tf
@@ -11,14 +11,14 @@ variable "vpc_cidr_block" {
 }
 
 variable "cidr_block_az_private" {
-    default     = ["172.32.0.0/24", "172.32.2.0/24"]
+    default     = ["172.32.1.0/24", "172.32.2.0/24"]
     type        = list
     description = "CIDR blocks for subnet private"
 }
 
 variable "cidr_block_az_public" {
-    default     = ["172.32.1.0/24", "172.32.3.0/24"]
-    type        = list(string)
+    default     = "172.32.0.0/24"
+    type        = string
     description = "CIDR blocks for subnet public"
 }
 
@@ -29,13 +29,13 @@ variable "cidr_block_l0" {
 }
 
 variable "az_private" {
-    default     = ["us-west-2a", "us-west-2c"]
+    default     = ["us-west-2b", "us-west-2c"]
     type        = list(string)
     description = "private AZs"
 }
 
 variable "az_public" {
-    default     = ["us-west-2b", "us-west-2d"]
-    type        = list(string)
+    default     = "us-west-2a"
+    type        = string
     description = "public AZs"
 }
